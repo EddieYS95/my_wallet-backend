83d7ae42aa928b44202edfde1cde0460ef993609
Yunsang Jeong <kunka8271@gmail.com>
2022-08-28 22:03:59 +0900

    feat: 예외 상황 추가

--- src/main/java/io/heachi/backend/exception/LogicErrorList.java
+++ src/main/java/io/heachi/backend/exception/LogicErrorList.java
@@ -9,8 +9,9 @@ public enum LogicErrorList {
 
   Invalid_Password(100, "Short_Password", "패스워드는 16자의 숫자 + 영문."),
   Fail_Transaction(200, "Fail_Transaction", "트랜잭션 요청에 실패했습니다."),
+  NotEnough_Balance(201, "NotEnough_Balance", "지갑의 잔액이 부족합니다."),
   DoesNotExit_Wallet(-1, "DoesNotExit_Wallet", "지갑을 찾을 수 없습니다."),
-  DoesNotExit_Transaction(-2, "DoesNotExit_Wallet", "지갑을 찾을 수 없습니다.");
+  DoesNotExit_Transaction(-2, "DoesNotExit_Transaction", "트랜잭션을 찾을 수 없습니다.");
 
   private final int errorCode;
   private final String errorMsg;
