1c6015c967a661130e0192ccd51c7edd1937fb4c
Yunsang Jeong <kunka8271@gmail.com>
2022-08-28 15:43:19 +0900

    test: 테스트 기본 틀 추가

---
+++ src/test/java/io/heachi/backend/infra/blockchain/EthereumTest.java
@@ -0,0 +1,23 @@
+package io.heachi.backend.infra.blockchain;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+class EthereumTest {
+
+  Ethereum ethereum = new Ethereum();
+
+//  @Test
+//  public void createWallet(){
+//    ethereum.createWallet();
+//  }
+
+  @Test
+  public void sendTransaction() throws IOException {
+    ethereum.transfer("0x47d3decb1a1cdef7a3011d1c278b8a5cfe767cea",
+        "e1b0daeddfdfe6b860929fe89e549506523f8407468d83fa649220df3d76d179",
+        "0xFDA13da0FB90999F9544971bC2fF87DE9c525288",
+        BigDecimal.valueOf(0.01));
+  }
+}
