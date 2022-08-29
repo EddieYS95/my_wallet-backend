49eb4ffc430c942d064e74e1d3ffa8e419430b9e
Yunsang Jeong <kunka8271@gmail.com>
2022-08-29 15:36:49 +0900

    feat: 에러 코드 추가

--- src/main/java/io/heachi/backend/exception/LogicErrorList.java
+++ src/main/java/io/heachi/backend/exception/LogicErrorList.java
@@ -10,6 +10,8 @@ public enum LogicErrorList {
   Invalid_Password(100, "Short_Password", "패스워드는 16자의 숫자 + 영문."),
   Fail_Transaction(200, "Fail_Transaction", "트랜잭션 요청에 실패했습니다."),
   NotEnough_Balance(201, "NotEnough_Balance", "지갑의 잔액이 부족합니다."),
+  NotEnough_ChainBalance(202, "NotEnough_ChainBalance",
+      "출금가능한 지갑의 잔액이 부족합니다. PENDING중이거나 MINED상태의 트랜잭션을 확인해 주세요."),
   DoesNotExit_Wallet(-1, "DoesNotExit_Wallet", "지갑을 찾을 수 없습니다."),
   DoesNotExit_Transaction(-2, "DoesNotExit_Transaction", "트랜잭션을 찾을 수 없습니다.");
 
