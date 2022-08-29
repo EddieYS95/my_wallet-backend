665c5389fe9b846e6a90f1821a787c2506f66340
Yunsang Jeong <kunka8271@gmail.com>
2022-08-29 15:37:01 +0900

    feat: 테이블이름 소문자로 변경

--- src/main/java/io/heachi/backend/domain/wallet/Wallet.java
+++ src/main/java/io/heachi/backend/domain/wallet/Wallet.java
@@ -17,7 +17,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
-@Table(name = "Wallet")
+@Table(name = "wallet")
 @Builder
 @AllArgsConstructor
 @Getter
